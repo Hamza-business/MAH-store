d7ab634a45f0778a9c890b091297dd428bc548bf
Hamza Elkotp <hamzadev1business@gmail.com>
2022-01-27 17:10:54 +0300

    all files

---
+++ public/assets/js/custom.js
@@ -0,0 +1,232 @@
+jQuery( document ).ready(function( $ ) {
+
+
+	"use strict";
+
+
+    
+        $(function() {
+            $( "#tabs" ).tabs();
+        });
+
+
+        // Page loading animation
+
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 600, function(){
+            setTimeout(function(){
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        });
+        
+
+        $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          var box = $('.header-text').height();
+          var header = $('header').height();
+
+          if (scroll >= box - header) {
+            $("header").addClass("background-header");
+          } else {
+            $("header").removeClass("background-header");
+          }
+        });
+        if ($('.owl-clients').length) {
+            $('.owl-clients').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 3,
+                        margin: 20
+                    },
+                    992: {
+                        items: 5,
+                        margin: 30
+                    }
+                }
+            });
+        }
+		if ($('.owl-testimonials').length) {
+            $('.owl-testimonials').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 2,
+                        margin: 30
+                    }
+                }
+            });
+        }
+        if ($('.owl-banner').length) {
+            $('.owl-banner').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 0,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 1,
+                        margin: 20
+                    },
+                    992: {
+                        items: 1,
+                        margin: 30
+                    }
+                }
+            });
+        }
+
+        $(".Modern-Slider").slick({
+            autoplay:true,
+            autoplaySpeed:10000,
+            speed:600,
+            slidesToShow:1,
+            slidesToScroll:1,
+            pauseOnHover:false,
+            dots:true,
+            pauseOnDotsHover:true,
+            cssEase:'linear',
+           // fade:true,
+            draggable:false,
+            prevArrow:'<button class="PrevArrow"></button>',
+            nextArrow:'<button class="NextArrow"></button>', 
+        });
+
+        $('.filters ul li').click(function(){
+        $('.filters ul li').removeClass('active');
+        $(this).addClass('active');
+          
+          var data = $(this).attr('data-filter');
+          $grid.isotope({
+            filter: data
+          })
+        });
+
+        var $grid = $(".grid").isotope({
+          itemSelector: ".all",
+          percentPosition: true,
+          masonry: {
+            columnWidth: ".all"
+          }
+        })
+        $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+
+        $('.accordion a').click(function(j) {
+            var dropDown = $(this).closest('li').find('.content');
+
+            $(this).closest('.accordion').find('.content').not(dropDown).slideUp();
+
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+            } else {
+                $(this).closest('.accordion').find('a.active').removeClass('active');
+                $(this).addClass('active');
+            }
+
+            dropDown.stop(false, true).slideToggle();
+
+            j.preventDefault();
+        });
+ 
+});
+
+
+
+document.getElementById('navigaton').innerHTML = (`
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="index.html"><h2>M<em>A</em>H</h2></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <a class="nav-link" href="index.html">الرئيسية</a>
+            </li> 
+            <li class="nav-item">
+            <a class="nav-link" href="products.html">منتجاتنا</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="about.html">من نحن</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="contact.html">تواصل معنا</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
+`);
+
+document.getElementById('footer').innerHTML = (`
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="inner-content">
+            <p>Copyright &copy; 2022 - Lifetime. - Developed By: 
+            <a rel="nofollow noopener" href="https://linktr.ee/HamzaPortfolio" target="_blank">Hamza Mohammed</a></p>
+            <p class="cont"><a href="https://linktr.ee/HamzaPortfolio">احصل على متجر مماثل</a></p>
+        </div>
+        </div>
+    </div>
+</div>
+`);
+
+
+cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+function clearField(t){                   //declaring the array outside of the
+if(! cleared[t.id]){                      // function makes it static and global
+    cleared[t.id] = 1;  // you could use true and false, but that's more typing
+    t.value='';         // with more chance of typos
+    t.style.color='#fff';
+    }
+}
+
